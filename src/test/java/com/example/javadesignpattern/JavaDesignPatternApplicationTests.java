1419c90c9e16696d15d7cfafae0d03a78d993c9d
jae518 <jaeyeon_kim@bigin.io>
2022-10-03 20:59:00 +0900

    Simple Factory (간단한 팩토리 패턴)

--- src/test/java/com/example/javadesignpattern/JavaDesignPatternApplicationTests.java
+++ src/test/java/com/example/javadesignpattern/JavaDesignPatternApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.javadesignpattern;
 
+import com.example.javadesignpattern.simplefactory.PizzaStore;
+import com.example.javadesignpattern.simplefactory.SimplePizzaFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaDesignPatternApplicationTests {
-
   @Test
   void contextLoads() {
+    SimplePizzaFactory simplePizzaFactory = new SimplePizzaFactory();
+
+    PizzaStore pizzaStore = new PizzaStore(simplePizzaFactory);
+
+    pizzaStore.orderPizza("cheese");
+
+    System.out.println("====================================");
+
+    pizzaStore.orderPizza("veggie");
   }
 
 }
